9b172d2ecff34fd988d868e8de5b903a3ecd85c9
H3ra-57 <jeffrey.namnum@gmail.com>
2025-02-26 13:57:46 -0500

    fire arrow adjustment

--- src/EDIT/mod.rs
+++ src/EDIT/mod.rs
@@ -35,7 +35,7 @@ unsafe extern "C" fn effect_appealhi(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         if TAUNT_EFFECT[entry_id] {
             if ARROW_TYPE[entry_id] == 1 {
-                EFFECT_FOLLOW(agent, Hash40::new("sys_flame"), Hash40::new("havel"), 0, 0.6, 2.3, 0, 0, 0, 1, true);
+                EFFECT_FOLLOW(agent, Hash40::new("sys_flame"), Hash40::new("top"), 0, 0.6, 2.3, 0, 0, 0, 1, true);
             }
         }
     }
@@ -173,15 +173,17 @@ unsafe extern "C" fn game_fly(agent: &mut L2CAgentBase) {
 
     let element = if arrow_type == 1 { Hash40::new("collision_attr_fire") } else if arrow_type == 2 { Hash40::new("collision_attr_ice") } else if arrow_type == 3 { Hash40::new("collision_attr_paralyze") } else { Hash40::new("collision_attr_sting") };
     let sound = if arrow_type == 1 { *COLLISION_SOUND_ATTR_FIRE } else if arrow_type == 2 { *COLLISION_SOUND_ATTR_FREEZE } else if arrow_type == 3 { *COLLISION_SOUND_ATTR_ELEC } else { *COLLISION_SOUND_ATTR_CUTUP };
-    let damage = if arrow_type == 1 { 8.0 } else { 5.0 };
+    let angle = if arrow_type == 1 { 20 } else { 361 };
+    let bkb = if arrow_type == 1 { 30 } else { 0 };
+    let kbg = if arrow_type == 1 { 20 } else { 0 };
     let hitlag = if arrow_type == 3 { 0.5 } else { 0.8 };
     if WorkModule::get_int(boma, *WN_LINK_BOWARROW_INSTANCE_WORK_ID_INT_SHOOT_NUM) <= 0 {
         if is_excute(agent) {
-            ATTACK(agent, 0, 0, Hash40::new("top"), damage, 361, 71, 0, 10, 1.35, 0.0, 0.0, 0.0, None, None, None, hitlag, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, element, *ATTACK_SOUND_LEVEL_S, sound, *ATTACK_REGION_OBJECT);
+            ATTACK(agent, 0, 0, Hash40::new("top"), 5.0, angle, 71 + kbg, 0, 10 + bkb, 1.35, 0.0, 0.0, 0.0, None, None, None, hitlag, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, element, *ATTACK_SOUND_LEVEL_S, sound, *ATTACK_REGION_OBJECT);
         }
         else {
             if is_excute(agent) {
-                ATTACK(agent, 0, 0, Hash40::new("top"), damage, 361, 110, 0, 25, 1.35, 0.0, 0.0, 0.0, None, None, None, hitlag, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, element, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_OBJECT);
+                ATTACK(agent, 0, 0, Hash40::new("top"), 5.0, angle, 110 + kbg, 0, 25 + bkb, 1.35, 0.0, 0.0, 0.0, None, None, None, hitlag, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, element, *ATTACK_SOUND_LEVEL_M, sound, *ATTACK_REGION_OBJECT);
                 AttackModule::enable_safe_pos(boma);
             }
         }
@@ -247,19 +249,23 @@ pub unsafe fn get_param_int_hook(x0: u64, x1: u64, x2 :u64) -> i32 {
     let mut boma = *((x0 as *mut u64).offset(1)) as *mut BattleObjectModuleAccessor;
     let boma_reference = &mut *boma;
     let fighter_kind = smash::app::utility::get_kind(boma_reference);
-    let id = WorkModule::get_int(boma, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
-    if smash::app::utility::get_category(boma_reference) == *BATTLE_OBJECT_CATEGORY_WEAPON && fighter_kind == *WEAPON_KIND_LINK_BOWARROW {
-        if ARROW_TYPE[id] == 1 {
-            if x1 == hash40("param_bowarrow") {
-                if x2 == hash40("power_min") {
-                    return 6;
-                }
-                else if x2 == hash40("power_max") {
-                    return 20;
+    if smash::app::utility::get_category(boma_reference) == *BATTLE_OBJECT_CATEGORY_WEAPON { 
+        let owner_module_accessor = &mut *sv_battle_object::module_accessor((WorkModule::get_int(boma, *WEAPON_INSTANCE_WORK_ID_INT_LINK_OWNER)) as u32);
+        if fighter_kind == *WEAPON_KIND_LINK_BOWARROW {
+            let id = smash::app::lua_bind::WorkModule::get_int(owner_module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
+            if ARROW_TYPE[id] == 1 {
+                if x1 == hash40("param_bowarrow") {
+                    if x2 == hash40("power_min") {
+                        return 6;
+                    }
+                    else if x2 == hash40("power_max") {
+                        return 20;
+                    }
                 }
             }
         }
     }
+
     original!()(x0, x1, x2)
 }
 
