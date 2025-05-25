52a4852b54d0dbc1e8424954cb7389e4bd18c7a1
H3ra-57 <jeffrey.namnum@gmail.com>
2025-05-25 12:21:24 -0400

    50% and only affects snake's fighter_kind

--- src/EDIT/mod.rs
+++ src/EDIT/mod.rs
@@ -19,15 +19,18 @@ unsafe extern "C" fn fighter_frame(fighter: &mut L2CFighterCommon) {
 }
 
 #[skyline::hook(offset = 0x68d7e0)]
-unsafe extern "C" fn snake_on_hit(vtable: u64, fighter: &mut Fighter, damage: f32) {
+unsafe extern "C" fn snake_on_hit(vtable: u64, fighter: &mut Fighter, damage: f32, fighter_kind: i32) {
     let object = &mut fighter.battle_object;
     let module_accessor = (*object).module_accessor;
     let motion = MotionModule::motion_kind(module_accessor);
-    if MOTION.contains(&motion) {
-        let heal = damage * -0.2;
-        DamageModule::add_damage(module_accessor, heal, 0);
+    let kind = WorkModule::get_int(module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_KIND);
+    if kind == *FIGHTER_KIND_SNAKE {
+        if MOTION.contains(&motion) {
+            let heal = damage * -0.5;
+            DamageModule::add_damage(module_accessor, heal, 0);
+        }
     }
-    original!()(vtable, fighter, damage);
+    original!()(vtable, fighter, damage, fighter_kind);
 }
 
 pub fn install() {
