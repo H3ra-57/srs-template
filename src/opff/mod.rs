e8feaf823290a84ab8c05a6aef9bdd70e51e0774
H3ra-57 <jeffrey.namnum@gmail.com>
2025-07-25 14:19:10 -0400

    extra setup

--- src/opff/mod.rs
+++ src/opff/mod.rs
@@ -23,12 +23,5 @@ unsafe extern "C" fn example_status_script(fighter: &mut L2CFighterCommon) -> L2
 }
 
 pub fn install() {
-    Agent::new("mario")
-        .game_acmd("game_ATTACK_NAME_HERE", example_acmd_script, Default) // Game acmd script
-        .on_line(Main, fighter_frame) // Char opff
-        .status(Main, *FIGHTER_MARIO_STATUS_KIND_SPECIAL_LW_CHARGE, example_status_script) // Status script
-        .install();
-    Agent::new("fighter")
-        .on_line(Main, fighter_frame) // Global opff
-        .install();
+    
 }
